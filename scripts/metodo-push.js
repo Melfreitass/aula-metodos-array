2680b2c5763f3681e5ebdf2433f32c8157eeb035
Melfreitass <melissa.g.freitas@aluno.senai.br>
2025-05-23 09:00:41 -0300

    feat: usa o método push e edita o arquivo README.md

---
+++ scripts/metodo-push.js
@@ -0,0 +1,10 @@
+let playlist = ["Funk", "Hip-Hop", "Pop"];
+
+console.log("Antes do push");
+console.table(playlist);
+
+//Adicionando um novo estilo na playlist
+playlist.push("Trap");
+
+console.log("Depois do push");
+console.table(playlist);
